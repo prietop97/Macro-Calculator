34be9fbcd0ba9a5a36242a385ac44b91d19353c6
Pedro Prieto <pedro.prieto003@gmail.com>
2020-06-24 19:28:10 -0400

    DTO

---
+++ Application/MainDTOs/MealDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.MainDTOs
+{
+    public class MealDto
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+
+    }
+}
